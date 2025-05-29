2acc5dd6e65052553046028bc3d702ac1f404562
Pandaroux007 <arduinoremi@gmail.com>
2025-05-29 17:22:16 +0200

    added two information commands, created the constants.py file

---
+++ src/constants.py
@@ -0,0 +1,14 @@
+# emojis
+CHECK = ":white_check_mark:"
+ERROR = ":x:"
+
+# texts
+REPULS_PRIVACY_LINK = "https://docskigames.com/privacy/"
+REPULS_LINK = "https://repuls.io/"
+FOOTER_EMBED = "repuls.io is developed with ♥️ by docski"
+REPULS_DESCRIPTION = f"""
+[Repuls.io]({REPULS_LINK}) is the future of browser games.
+The best free instantly accessible multiplayer first-person shooter for your browser with no sign-up or payment required!
+
+Tired of the same run, aim, shoot gameplay that every shooter does??! Played one, you played them all! Repuls has you riding bikes, grappling cliffs, piloting mechs and firing miniguns and plasma rifles and stomping vehicles with a giant mech!! THATS the repuls experience son!
+"""
--- src/main.py
+++ src/main.py
@@ -2,11 +2,9 @@ import discord
 from discord.ext import commands
 # IDs and TOKEN file
 from private_data import *
+from constants import *
 
-CHECK = ":white_check_mark:"
-ERROR = ":x:"
-
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), help_command=None)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 def admin_or_roles():
     async def predicate(ctx: commands.Context):
@@ -16,9 +14,50 @@ def admin_or_roles():
     return commands.check(predicate)
 
 # ---------------------------------- commands
-@bot.hybrid_command(name="ping")
+@bot.hybrid_command(name="ping", description="displays latency of the bot")
 async def ping(ctx: commands.Context):
     await ctx.send(f"{CHECK} **pong!**\n(*It took me {round(bot.latency * 1000, 2)}ms to respond to your command!*)")
+
+@bot.hybrid_command(name="aboutserver", description="displays information about the server")
+async def aboutserver(ctx: commands.Context):
+    guild = ctx.guild
+    embed = discord.Embed(
+        title=f"About *{guild.name}* server",
+        description="Information about this discord server",
+        color=discord.Color.blue(),
+    )
+    embed.set_thumbnail(url = guild.icon.url if guild.icon else None)
+    embed.add_field(name="Owner", value=guild.owner.mention, inline=True)
+    embed.add_field(name="Created on", value=guild.created_at.strftime("%d/%m/%Y %H:%M"), inline=True)
+    embed.add_field(name="Members", value=guild.member_count, inline=True)
+    embed.add_field(name="Channels", value=f"{len(guild.text_channels)} Text | {len(guild.voice_channels)} Voice", inline=True)
+    embed.add_field(name="Roles", value=len(guild.roles), inline=True)
+    embed.add_field(name="Emojis", value=len(guild.emojis), inline=True)
+    embed.add_field(name="Description", value=guild.description, inline=False)
+    embed.set_footer(text=FOOTER_EMBED)
+    
+    await ctx.send(embed=embed)
+
+@bot.hybrid_command(name="game", description="displays information about repuls.io game")
+async def game(ctx: commands.Context):
+    play_btn_view = discord.ui.View()
+    play_btn = discord.ui.Button(
+        style=discord.ButtonStyle.link,
+        label="PLAY NOW!",
+        url=REPULS_LINK
+    )
+    play_btn_view.add_item(play_btn)
+    embed = discord.Embed(
+        title=f"What is repuls.io ?",
+        url=f"{REPULS_LINK}home",
+        description=REPULS_DESCRIPTION,
+        color=discord.Color.dark_blue(),
+    )
+    embed.add_field(name="Leaderboard", value=f"[Leaderboard]({REPULS_LINK}leaderboard)", inline=True)
+    embed.add_field(name="Updates", value=f"[Updates]({REPULS_LINK}updates)", inline=True)
+    embed.add_field(name="Terms & privacy", value=f"[Privacy]({REPULS_PRIVACY_LINK})", inline=True)
+    embed.set_footer(text=FOOTER_EMBED)
+    await ctx.send(embed=embed, view=play_btn_view)
 
 @bot.tree.command(name="clean", description="allows you to clean a certain number of messages in a channel")
 @admin_or_roles()
